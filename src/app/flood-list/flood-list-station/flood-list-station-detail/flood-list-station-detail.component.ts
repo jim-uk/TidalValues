020446bce9d82e3bf5890fad380dc03bb7aa23d8
jim-uk <60322889+jim-uk@users.noreply.github.com>
2021-05-24 19:39:50 +0100

    Improved layout to remove horizontal scroll bar

--- src/app/flood-list/flood-list-station/flood-list-station-detail/flood-list-station-detail.component.ts
+++ src/app/flood-list/flood-list-station/flood-list-station-detail/flood-list-station-detail.component.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FloodDataService } from '../../flood-data.service';
 import { StationDetail } from '../../stationDetail.modal';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-flood-list-station-detail',
   templateUrl: './flood-list-station-detail.component.html',
   styleUrls: ['./flood-list-station-detail.component.css']
 })
-export class FloodListStationDetailComponent implements OnInit {
+export class FloodListStationDetailComponent implements OnInit, OnDestroy {
 
   public loading: boolean = true;
 
   public stationDetail: StationDetail;
+  public getDetailSubject: Subscription;
+  id: string;
 
   constructor(private floodDataService: FloodDataService,
     private route: ActivatedRoute,
     private router: Router) { }
-
-  recipe: FloodListStationDetailComponent;
-  id: string;
 
   ngOnInit(): void {
     console.log("Into station detail");
@@ -31,9 +31,9 @@ export class FloodListStationDetailComponent implements OnInit {
         //fetch the detail for this Station (another fetch request & subject)
         //this.recipe=this.recipeService.getRecipe(this.id);
 
-        this.floodDataService.fetchStationDetail(this.id).subscribe();
+        this.getDetailSubject=this.floodDataService.fetchStationDetail(this.id).subscribe();
 
-      }
+        }
     );
 
     this.floodDataService.stationDetailSubject.subscribe( () => {
@@ -41,6 +41,10 @@ export class FloodListStationDetailComponent implements OnInit {
       console.log("Got Station " + this.stationDetail);
       this.loading=false;
     })
+  }
+
+  ngOnDestroy(){
+    this.getDetailSubject.unsubscribe();
   }
 
 }
